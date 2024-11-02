aca706e94450e105cb6b927621b511eb48c19c0c
borajin <borajin.soft@gmail.com>
2024-11-03 03:17:53 +0900

    🩹 chor: layout 에서 user 조회하는 로직 변경

--- src/routes/category/+layout.server.ts
+++ src/routes/category/+layout.server.ts
@@ -1,6 +1,11 @@
 import type { LayoutServerLoad } from "./$types";
+import { redirect } from "@sveltejs/kit";
 
 export const load: LayoutServerLoad = async (event) => {
+  if (event.locals.user) {
+    return redirect(302, "/");
+  }
+
   return {
     user: event.locals.user,
   };
